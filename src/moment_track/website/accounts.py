be8072611e4bc1e39ae1e87c506c1fa798bd02f9
Francesco Marano <francesco.mrn24@gmail.com>
2017-06-11 20:29:17 +0200

    Added function to extract user displayable name to django-allauth

---
+++ src/moment_track/website/accounts.py
@@ -0,0 +1,14 @@
+def user_displayable_name(user):
+    """
+    Use some basic information to build a string representing the given user.
+    :param user: the user you want a displayable name
+    :type user: settings.AUTH_USER_MODEL(=django.contrib.auth.models.User)
+    :return: the user displayable name
+    :rtype: str
+    """
+    if user.first_name or user.last_name:
+        displayable_name = ' '.join((user.first_name, user.last_name))
+    else:
+        displayable_name = user.email[:user.email.find('@')]
+
+    return displayable_name
