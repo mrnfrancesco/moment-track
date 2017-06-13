18ea162d155851cd897ec03a2132195c3c6c6775
Francesco Marano <francesco.mrn24@gmail.com>
2017-06-13 12:59:12 +0200

    Created company user signup view with django-allauth compatibility

--- src/moment_track/website/views.py
+++ src/moment_track/website/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from allauth.account.views import SignupView
 from django.shortcuts import render
 
-# Create your views here.
+from website.forms import CompanySignupForm
 
 
 def home(request):
@@ -16,3 +17,11 @@ def privacy_policy(request):
 
 def terms_of_service(request):
     return render(request, 'website/terms-of-service.html')
+
+
+class CompanyUserSignupView(SignupView):
+    template_name = 'account/signup_company.html'
+    form_class = CompanySignupForm
+    view_name = 'website:company_signup'
+
+company_signup = CompanyUserSignupView.as_view()
