09f49c12865056ea5a96b8ff3f05b6667af8b285
Francesco Marano <francesco.mrn24@gmail.com>
2017-06-25 16:40:44 +0200

    Refactored db preparation script for instances deletion and validation

--- src/moment_track/preparedb.py
+++ src/moment_track/preparedb.py
@@ -5,22 +5,31 @@ from django.contrib.sites.models import Site
 from allauth.socialaccount.providers.dropbox_oauth2.provider import DropboxOAuth2Provider
 from allauth.socialaccount.models import SocialApp
 
+from dashboard.models import User, CreditsPacketOffer
 
-# Create site domain
+for ModelCls in (User, Site, SocialApp, CreditsPacketOffer):
+    print("Deleting %s..." % ModelCls.__name__, end='')
+    ModelCls.objects.all().delete()
+    print("OK")
+
 print("Creating site domain...", end='')
-site, _ = Site.objects.get_or_create(pk=1)
-site.domain = '127.0.0.1:9000'
-site.name = 'Moment Track'
+site = Site(
+    pk=1,
+    domain='127.0.0.1:9000',
+    name='Moment Track'
+)
+site.full_clean()
 site.save()
 print("OK")
 
-# Register Dropbox OAuth2 social app
 print("Registering Dropbox OAuth2 social app...", end='')
-social_app, _ = SocialApp.objects.get_or_create(pk=1)
-social_app.provider = DropboxOAuth2Provider.id
-social_app.name = 'Dropbox OAuth2'
-social_app.client_id = 'zq6xbpqx3zh2i00'
-social_app.secret = 'pkyu37aqhmgw9fs'
+social_app = SocialApp(
+    provider=DropboxOAuth2Provider.id,
+    name='Dropbox OAuth2',
+    client_id='zq6xbpqx3zh2i00',
+    secret='pkyu37aqhmgw9fs'
+)
+social_app.full_clean()
 social_app.save()
 social_app.sites.add(site)
 social_app.save()
