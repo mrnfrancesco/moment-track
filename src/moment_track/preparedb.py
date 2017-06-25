83e6c5656defb4191f45840f111211912da317ae
Francesco Marano <francesco.mrn24@gmail.com>
2017-06-25 16:41:28 +0200

    Added sample credits packet offer in db preparation script

--- src/moment_track/preparedb.py
+++ src/moment_track/preparedb.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+from datetime import date, timedelta
+
 from django.contrib.sites.models import Site
 
 from allauth.socialaccount.providers.dropbox_oauth2.provider import DropboxOAuth2Provider
@@ -35,4 +37,18 @@ social_app.sites.add(site)
 social_app.save()
 print("OK")
 
-print("Done")
+print("Creating sample credits packet offers...", end='')
+credits_packet_offers = [
+    CreditsPacketOffer(pk=1, minutes_per_credit=10, cost_per_credit=0.70),
+    CreditsPacketOffer(pk=2, minutes_per_credit=15, cost_per_credit=0.85),
+    CreditsPacketOffer(pk=3, minutes_per_credit=30, cost_per_credit=1.89, date_end=(date.today() + timedelta(days=15))),
+    CreditsPacketOffer(pk=4, minutes_per_credit=60, cost_per_credit=2.5, date_end=(date.today() + timedelta(days=1))),
+]
+
+for i, offer in enumerate(credits_packet_offers, start=1):
+    offer.full_clean()
+    offer.save()
+    print("%d..." % i, end='')
+print("OK")
+
+print("Done!")
