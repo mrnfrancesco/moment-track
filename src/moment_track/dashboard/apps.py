5c49436fb26fbb18b4ede5c5df7519e1f13b587e
Francesco Marano <francesco.mrn24@gmail.com>
2017-06-26 12:12:55 +0200

    Fixed app crash on startup

--- src/moment_track/dashboard/apps.py
+++ src/moment_track/dashboard/apps.py
@@ -5,13 +5,13 @@ from django.apps import AppConfig
 
 from paypal.standard.ipn.signals import valid_ipn_received
 
-from dashboard import signals
-
 
 class WebsiteConfig(AppConfig):
     name = 'dashboard'
 
     def ready(self):
+        from dashboard import signals
+
         valid_ipn_received.connect(
             signals.private_user_bought_credits_packet,
             dispatch_uid='private-user-bought-credits-packet'
