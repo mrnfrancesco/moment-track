2ec037efd2869e979e738ad948b91115cc8d3733
Francesco Marano <francesco.mrn24@gmail.com>
2017-06-26 11:51:31 +0200

    Added paypal ipn signal handling for credits update

--- src/moment_track/dashboard/apps.py
+++ src/moment_track/dashboard/apps.py
@@ -3,6 +3,16 @@ from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
+from paypal.standard.ipn.signals import valid_ipn_received
+
+from dashboard import signals
+
 
 class WebsiteConfig(AppConfig):
     name = 'dashboard'
+
+    def ready(self):
+        valid_ipn_received.connect(
+            signals.private_user_bought_credits_packet,
+            dispatch_uid='private-user-bought-credits-packet'
+        )
